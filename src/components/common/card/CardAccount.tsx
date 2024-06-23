a1cae51df0dbc3a046e7029ee50c2c34bc769bd9
petrushandika <petrushandikasinaga@gmail.com>
2024-06-23 22:32:38 +0700

    Edited Follow Button

--- src/components/common/card/CardAccount.tsx
+++ src/components/common/card/CardAccount.tsx
@@ -2,12 +2,18 @@ import { HStack, VStack, Text } from "@chakra-ui/react";
 import CardImage from "./CardImage";
 import HollowButton from "../button/HollowButton";
 import { User } from "../../../types/User";
+import { useState } from "react";
 
 interface UserProps {
   user: User;
 }
 
 export default function CardAccount({ user }: UserProps) {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const handleFollowClick = () => {
+    setIsFollowing(!isFollowing);
+  };
 
   return (
     <HStack width={"100%"} justify={"space-between"}>
@@ -19,15 +25,16 @@ export default function CardAccount({ user }: UserProps) {
           </Text>
           <Text color={"#909090"} fontSize={"sm"} fontWeight={"400"}>
             @{user.username}
-          </Text> 
+          </Text>
         </VStack>
       </HStack>
       <HStack>
         <HollowButton
-          text="Follow"
+          text={isFollowing ? "Following" : "Follow"}
           width={"100%"}
           height={"33px"}
           fontSize={".9rem"}
+          onClick={handleFollowClick}
         />
       </HStack>
     </HStack>
